18caabf4301c5d3273ec26c2f9f6400a080f91b1
malathi <poloju.malathi@gmail.com>
2021-01-15 16:02:22 +0530

    update by person 3 and commit

--- src/test/java/qaclickacademy/Mavenjava/RestAPITest.java
+++ src/test/java/qaclickacademy/Mavenjava/RestAPITest.java
@@ -10,11 +10,18 @@ public class RestAPITest {
 	{
 		System.out.println("Npostjira");
 		System.out.println("Npostjira cloned1");//updated code fir example
-		System.out.println("Npostjira cloned"2);
+		System.out.println("Npostjira cloned");
+		//now we update in this project and check in gitdemo master
+		System.out.println("Npostjira cloned3");
+		System.out.println("Npostjira cloned4");
+		System.out.println("Npostjira cloned5");
+		System.out.println("we automation");
 	}
 @Test
 public void twitterdelete()
 {
 	System.out.println("twitterdelete");
+	System.out.println("Npostjira cloned6");
+	System.out.println("Npostjira cloned7");
 }
 }
--- src/test/java/qaclickacademy/Mavenjava/seleniumTest.java
+++ src/test/java/qaclickacademy/Mavenjava/seleniumTest.java
@@ -12,5 +12,8 @@ public class seleniumTest {
 	@Test
 	public void elementsui() {
 		System.out.println("telementsui");
+		System.out.println("Npostjira cloned1");
+		System.out.println("Npostjira cloned6");
+		System.out.println("webs automation");
 	}
 }
